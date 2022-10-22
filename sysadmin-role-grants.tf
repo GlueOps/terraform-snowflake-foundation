6aa393898e169ef81c445b4d482414377913e4ae
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2022-10-22 00:12:21 +0000

    feat: dependency management for granting roles to sysadmin

--- sysadmin-role-grants.tf
+++ sysadmin-role-grants.tf
@@ -1,17 +1,22 @@
 #== Grant All Service User and Principal Roles to SYSADMIN so they can be managed
 locals {
-  all_roles = merge(
-    var.principal_roles,
-    var.service_users
+  all_roles = concat(
+    [for k, v in snowflake_role.principal_roles : v.name],
+    [for k, v in snowflake_role.service_user_roles : v.name],
   )
 }
 
 resource "snowflake_role_grants" "grant_all_user_roles_to_sysadmin" {
   provider = snowflake
-  for_each = local.all_roles
+  for_each = toset(local.all_roles)
 
   role_name = each.key
 
   roles = ["SYSADMIN"]
   users = []
+
+  depends_on = [
+    snowflake_role.principal_roles,
+    snowflake_role.service_user_roles
+  ]
 }
