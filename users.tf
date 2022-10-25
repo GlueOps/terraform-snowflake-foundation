e970bc2ea934e6c4137de0fefbea441af3efef9d
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2022-10-25 23:40:49 +0000

    fix: complete grants and correct grant direction

--- users.tf
+++ users.tf
@@ -1,3 +1,4 @@
+#=== Configure Human Users
 resource "snowflake_user" "human_users" {
   provider = snowflake
   for_each = var.human_users
@@ -7,6 +8,7 @@ resource "snowflake_user" "human_users" {
   default_role         = snowflake_role.principal_roles[each.value[0]].name
 }
 
+#= Principal Roles for Human Users
 resource "snowflake_role" "principal_roles" {
   provider = snowflake
   for_each = var.principal_roles
@@ -14,15 +16,44 @@ resource "snowflake_role" "principal_roles" {
   name = each.key
 }
 
-resource "snowflake_role_grants" "principal_role_grants" {
+locals {
+  roles_to_grant_to_human_users = distinct(flatten([
+    for k, v in snowflake_user.human_users : [
+      for granted_role in var.human_users[k] : {
+        target_user  = k
+        granted_role = granted_role
+      }
+    ]
+  ]))
+}
+resource "snowflake_role_grants" "human_user_role_grants" {
   provider = snowflake
-  for_each = snowflake_role.principal_roles
+  for_each = { for role_map in local.roles_to_grant_to_human_users : "${role_map.target_user}-${role_map.granted_role}" => role_map }
 
-  role_name = each.key
-  roles     = var.principal_roles[each.key]
+  role_name = each.value.granted_role
+  users     = [each.value.target_user]
+
 }
 
+locals {
+  privilege_roles_to_grant_to_principal_roles = distinct(flatten([
+    for k, v in snowflake_role.principal_roles : [
+      for granted_role in var.principal_roles[k] : {
+        target_role    = k
+        privilege_role = granted_role
+      }
+    ]
+  ]))
+}
+resource "snowflake_role_grants" "principal_role_grants" {
+  provider = snowflake
+  for_each = { for role_map in local.privilege_roles_to_grant_to_principal_roles : "${role_map.target_role}-${role_map.privilege_role}" => role_map }
 
+  role_name = each.value.privilege_role
+  roles     = [each.value.target_role]
+}
+
+#=== Configure Service Users
 resource "snowflake_user" "service_users" {
   provider = snowflake
   for_each = var.service_users
@@ -34,7 +65,6 @@ resource "snowflake_user" "service_users" {
   default_warehouse = each.key
 }
 
-# Grant Read All Role to identified principals
 resource "snowflake_role" "service_user_roles" {
   provider = snowflake
   for_each = var.service_users
@@ -44,9 +74,27 @@ resource "snowflake_role" "service_user_roles" {
 
 resource "snowflake_role_grants" "service_user_role_grants" {
   provider = snowflake
-  # for_each = var.service_users
   for_each = snowflake_user.service_users
 
   role_name = each.key
   users     = [each.key]
+
+}
+
+locals {
+  roles_to_grant_to_service_users = distinct(flatten([
+    for k, v in snowflake_role.service_user_roles : [
+      for granted_role in var.service_users[k]["privilege_roles"] : {
+        target_role    = k
+        privilege_role = granted_role
+      }
+    ]
+  ]))
+}
+resource "snowflake_role_grants" "service_user_privilege_role_grants" {
+  provider = snowflake
+  for_each = { for role_map in local.roles_to_grant_to_service_users : "${role_map.target_role}-${role_map.privilege_role}" => role_map }
+
+  role_name = each.value.privilege_role
+  roles     = [each.value.target_role]
 }
